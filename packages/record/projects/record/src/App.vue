830ebaf7a52ea40a83a5c5aa5af61231bd3ac102
xiangweiping103 <xiangweiping103@pingan.com.cn>
2019-03-07 15:03:18 +0800

    vue

---
+++ packages/record/projects/record/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="name"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import utils from '@branonxiang/base-utils'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+
+  data()  {
+    return {
+      name: utils()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- packages/record/src/record/__tests__/record.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const record = require('..');
-
-describe('record', () => {
-    it('needs tests');
-});
